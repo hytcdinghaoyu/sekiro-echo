37bf6a3944a1afa6adbfc88c435cf900734c805f
dinghaoyu <a835399559@gmail.com>
2019-04-08 17:41:34 +0800

    add jwt

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module sekiro_echo
 go 1.12
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/labstack/echo/v4 v4.0.0
+	github.com/labstack/gommon v0.2.8
 	github.com/mattn/go-colorable v0.1.1 // indirect
 	github.com/mattn/go-isatty v0.0.7 // indirect
 	github.com/valyala/fasttemplate v1.0.1 // indirect
 	golang.org/x/crypto v0.0.0-20190313024323-a1f597ede03a // indirect
+	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
 )
--- server.go
+++ server.go
@@ -1,15 +1,52 @@
 package main
 
 import (
-	"net/http"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+	"sekiro_echo/handler"
+	"gopkg.in/mgo.v2"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+		SigningKey: []byte(handler.Key),
+		Skipper: func(c echo.Context) bool {
+			// Skip authentication for and signup login requests
+			if c.Path() == "/login" || c.Path() == "/signup" {
+				return true
+			}
+			return false
+		},
+	}))
+
+	// Database connection
+	db, err := mgo.Dial("localhost")
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+
+	// Create indices
+	if err = db.Copy().DB("twitter").C("users").EnsureIndex(mgo.Index{
+		Key:    []string{"email"},
+		Unique: true,
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	// Initialize handler
+	h := &handler.Handler{DB: db}
+
+	// Routes
+	e.POST("/signup", h.Signup)
+	e.POST("/login", h.Login)
+	e.POST("/follow/:id", h.Follow)
+	e.POST("/posts", h.CreatePost)
+	e.GET("/feed", h.FetchPost)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
-
