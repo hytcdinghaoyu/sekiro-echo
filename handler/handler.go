2deee80184fd2a606cf5975d4ee32f2272032e02
dinghaoyu <a835399559@gmail.com>
2019-05-13 15:02:28 +0800

    ch conf

--- handler/handler.go
+++ handler/handler.go
@@ -14,7 +14,7 @@ var (
 func Mongodb() *mgo.Session {
 	if mongodb == nil {
 		var err error
-		mongodb, err = mgo.Dial("localhost")
+		mongodb, err = mgo.Dial("124.77.180.237")
 		if err != nil {
 			log.Fatal(err)
 		}
