20983ea30f5e4f34eb15c109f256009f8c10fd43
dinghaoyu <a835399559@gmail.com>
2019-05-14 15:45:46 +0800

    ch route

--- app/idip/api/main.go
+++ app/idip/api/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	_ "sekiro_echo/app/idip/routers"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 	"github.com/micro/go-web"
 )
 
@@ -15,6 +17,10 @@ func main() {
 	)
 
 	service.Init()
+
+	beego.Get("/idip/test", func(ctx *context.Context) {
+		fmt.Println(212)
+	})
 
 	app := beego.BeeApp
 	// Register Handler
